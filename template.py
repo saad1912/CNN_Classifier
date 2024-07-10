8973b55e9b565d0cb3f31f4ac9ee8dd0f2b3de32
Saad Ahmad <saad1912@gmail.com>
2024-07-11 00:23:31 +0530

    git push origin master

--- template.py
+++ template.py
@@ -1,4 +1,42 @@
 import os
 from pathlib import Path
+import logging
 
-print(Path("x/y/z.txt"))
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+
+
+package_name ="deepClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/utils/__init__.py",
+    f"src/{package_name}/config/__init__.py",
+    f"src/{package_name}/pipeline/__init__.py",
+    f"src/{package_name}/entity/__init__.py",
+    f"src/{package_name}/constants/__init__.py",
+    "configs/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "research/trials.ipynb"
+
+
+    
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info()
+
